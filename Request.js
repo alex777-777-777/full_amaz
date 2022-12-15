3e775c3fa59a641a375c62d5948e38b839188d7d
alex777-777-777 <109542513+alex777-777-777@users.noreply.github.com>
2022-12-15 16:18:35 +0200

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,13 +1,4 @@
 const axios = require("axios");
-/*/
-const headers = {
-  "x-trackbox-username": "GLC",
-  "x-trackbox-password": "21yOpE3Y6r2S",
-  "x-api-key": "2643889w34df345676ssdas323tgc738",
-  "Content-Type": "application/json",
-  Origin: "http://localhost:3006",
-};
-/*/
 
 const postData = (d) => {
   const url = `https://platform.adscobar.com/api/signup/procform`;
@@ -19,9 +10,9 @@ const postData = (d) => {
     firstname: d.name,
     lastname: d.name,
     email: d.email,
-    password: "Aa12345!",
+    password: "123456Aa",
     phone: d.phone,
-    so: "amazon",
+    so: "AMAZON",
     sub: "FreeParam",
     MPC_1: d.answer,
     MPC_2: "FreeParam",
@@ -43,6 +34,7 @@ const postData = (d) => {
       "x-trackbox-password": "123456Aa",
       "x-api-key": "2643889w34df345676ssdas323tgc738",
       "Content-Type": "application/json",
+      "User-Agent": "WordPress/6.0.3; https://fox.tradergroupinc.space",
     },
     data: data,
   };
@@ -55,5 +47,34 @@ const postData = (d) => {
       console.log(error);
     });
 };
+const postToCRMFranklin = (d) => {
+  const url = `https://frnkaffs-api.com/api/v2/leads`;
 
-module.exports = { postData };
+  const data = new URLSearchParams({
+    email: d.email,
+    firstName: d.name,
+    lastName: d.name,
+    password: "Pas21word2",
+    phone: Number(d.phone),
+    ip: d.ip,
+    custom1: d.answer,
+  });
+  const requestOptions = {
+    method: "post",
+    url: url,
+    data: data,
+    headers: {
+      "Api-Key": "6D0D404D-DF26-1A5E-A88D-E0ECC6A4E17E ",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
+  axios(requestOptions)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+module.exports = { postData, postToCRMFranklin };
