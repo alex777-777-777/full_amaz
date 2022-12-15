4623e856a82c36be56af0b0e3f0a5f6bf74a13aa
alex777-777-777 <109542513+alex777-777-777@users.noreply.github.com>
2022-12-15 16:19:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const cors = require("cors");
-const { postData } = require("./Request.js");
+const { postData, postToCRMFranklin } = require("./Request.js");
 
 const port = process.env.PORT || 5000;
 app.use(
@@ -17,9 +17,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ users: ["ueserOne", "ueserTwo"] });
 });
-app.post("/quest", (req, res) => {
-  res.send(req.body);
-  postData(req.body);
+app.post("/quest", async (req, res) => {
+  try {
+    postData(req.body);
+    res.status(200).json("success");
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/franklin", async (req, res) => {
+  try {
+    await postToCRMFranklin(req.body);
+    res.status(200).json("success");
+  } catch (error) {
+    res.status(400).json("error mesage");
+    console.log(error);
+  }
 });
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
