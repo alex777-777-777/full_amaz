8947784da11945c8aa119d614e4c044ea065d285
alex777-777-777 <109542513+alex777-777-777@users.noreply.github.com>
2023-01-20 18:54:53 +0200

    Update Request.js

--- Request.js
+++ Request.js
@@ -157,10 +157,33 @@ const postDataToCrmTssuccess = (d) => {
     .then((res) => res)
     .catch((err) => err);
 };
+
+const postDataToCRMVikTeam = (d) => {
+  const url = `https://cryptcrm.site/api/ext/add.json?id=68-52e3ee9191f6f135e68b689a135516a6`;
+  const data = {
+    flow: "68-52e3ee9191f6f135e68b689a135516a6",
+    offer: 30,
+    ip: d.ip,
+    name: d.name,
+    last: d.name,
+    phone: d.phone,
+    email: d.email,
+    country: d.geo,
+  };
+  const requestOptions = {
+    method: "post",
+    url: url,
+    data: data,
+  };
+  return axios(requestOptions)
+    .then((res) => res)
+    .catch((err) => err);
+};
 module.exports = {
   postDataToCrmAdscobar,
   postToCRMFranklin,
   postDataToCrmHellLeads,
   postDataToCrmTssuccess,
+  postDataToCRMVikTeam,
 };
 
