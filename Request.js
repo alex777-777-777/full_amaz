9b23ad83dc3b9180c680795ccead51373814897a
alex777-777-777 <109542513+alex777-777-777@users.noreply.github.com>
2023-02-02 12:11:55 +0200

    Update Request.js

--- Request.js
+++ Request.js
@@ -150,7 +150,7 @@ const postDataToCRMVikTeam = (d) => {
   const url = `https://cryptcrm.site/api/ext/add.json?id=68-52e3ee9191f6f135e68b689a135516a6`;
   const data = {
     flow: "68-52e3ee9191f6f135e68b689a135516a6",
-    offer: 30,
+    offer: d.offerId,
     ip: d.ip,
     name: d.name,
     last: d.name,
