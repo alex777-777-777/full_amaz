f954ae1109ea3109c7a1b7d796419f9aceb96d23
alex777-777-777 <109542513+alex777-777-777@users.noreply.github.com>
2023-04-18 17:39:42 +0300

    Update Request.js

--- Request.js
+++ Request.js
@@ -66,7 +66,48 @@ const postDataToCrmAdscobar = (d) => {
     .then((res) => res)
     .catch((err) => err);
 };
+const postDataToCrmGlc = (d) => {
+  const url = `https://platform.adscobar.com/api/signup/procform`;
+  const data = JSON.stringify({
+    ai: "2958791",
+    ci: "1",
+    gi: "366",
+    userip: d.ip,
+    firstname: d.name,
+    lastname: d.sName,
+    email: d.email,
+    password: "123456Aa",
+    phone: d.phone,
+    so: d.land,
+    sub: "FreeParam",
+    MPC_1: d.id,
+    MPC_2: "FreeParam",
+    MPC_3: "FreeParam",
+    MPC_4: "FreeParam",
+    MPC_5: "FreeParam",
+    MPC_6: "FreeParam",
+    MPC_7: "FreeParam",
+    MPC_8: "FreeParam",
+    MPC_9: "FreeParam",
+    MPC_10: "FreeParam",
+  });
 
+  const requestOptions = {
+    method: "post",
+    url: url,
+    headers: {
+      "x-trackbox-username": "GLCWORLD",
+      "x-trackbox-password": "123456Aa",
+      "x-api-key": "2643889w34df345676ssdas323tgc738",
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  return axios(requestOptions)
+    .then((res) => res)
+    .catch((err) => err);
+};
 const postDataToCrmCoinvista = (d) => {
   const url = `https://cv.coinsvista.com/api/signup/procform`;
   const data = JSON.stringify({
@@ -445,5 +486,6 @@ module.exports = {
   postDataToCrmLeadShot,
   postToGoogleSheet,
   postDataToCrmCoinvista,
-  postDataToCrmСelineMarketing
+  postDataToCrmСelineMarketing,
+  postDataToCrmGlc
 };
